0af3e54a7a9eb9960a0212c5d10257dad1102d82
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-12-07 14:34:48 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,14 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  #returning a cart where coupons are appled on th :price 
+  index = 0 
+  new_cart = [ ]
+  cart.each do |grocery_item|
+    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    if find_item_by_name_in_collection
+  
+ return new_cart
 end
 
 def apply_clearance(cart)
